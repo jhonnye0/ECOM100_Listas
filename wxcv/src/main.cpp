2af1126a386f856f6b0d0f78681f3b54d8d4c8c5
Valerio Nogueira <vnrj@ic.ufal.br>
2020-11-03 19:08:56 -0300

    Ligando eventos da barra de menu, adicionando a funcionalidade de abrir e salvar imagens e adicionando documentação das funções

--- wxcv/src/main.cpp
+++ wxcv/src/main.cpp
@@ -4,10 +4,14 @@
 
 #include "wxCV.h"
 #include <wx/menu.h>
+#include <wx/filedlg.h>
+#include <wx/stdpaths.h>
 
 
 namespace wxcv {
-
+    /*
+     * Create a MainWindow and show it
+     */
     bool wxCV::OnInit() {
         MainWindow* mainWindow = new MainWindow();
         mainWindow->Show(true);
@@ -16,9 +20,13 @@ namespace wxcv {
 
     MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "wxCV") {
         InitMenu();
+        BindEvents();
         Center();
     }
 
+    /*
+     * Create the menu bar and attach it to the window
+     */
     void MainWindow::InitMenu() {
         wxMenuBar* bar = new wxMenuBar();
         wxMenu* file = new wxMenu();
@@ -48,6 +56,74 @@ namespace wxcv {
 
         SetMenuBar(bar);
         CreateStatusBar();
+    }
+
+    /*
+     * Bind each menu bar item
+     */
+    void MainWindow::BindEvents() {
+        Bind(wxEVT_MENU, &MainWindow::OpenImage, this, ID_OPEN_IMAGE);
+        Bind(wxEVT_MENU, &MainWindow::SaveImage, this, ID_SAVE_IMAGE);
+        Bind(wxEVT_MENU, &MainWindow::Exit, this, wxID_EXIT);
+        Bind(wxEVT_MENU, &MainWindow::ResizeImage, this, ID_RESIZE_IMAGE);
+        Bind(wxEVT_MENU, &MainWindow::Filter2D, this, ID_FILTER_2D);
+        Bind(wxEVT_MENU, &MainWindow::About, this, wxID_ABOUT);
+    }
+
+
+    /*
+     * Prompt the user to choose an image from the file explorer and load it into @image with OpenCV
+     */
+    void MainWindow::OpenImage(wxCommandEvent& event) {
+        wxFileDialog fileDialog(this, wxT("Abrir imagem"));
+
+        if(fileDialog.ShowModal() == wxID_CANCEL){
+            return;
+        }
+
+        image = cv::imread(fileDialog.GetPath().ToStdString());
+        if(!image.empty()){
+            SetStatusText(fileDialog.GetFilename() + wxT(" carregada com sucesso!"));
+        }
+    }
+
+
+    /*
+     * Prompt the user to save the image in disk using OpenCV
+     */
+    void MainWindow::SaveImage(wxCommandEvent& event) {
+        wxFileDialog fileDialog(this, wxT("Salvar imagem"));
+
+        if(fileDialog.ShowModal() == wxID_CANCEL){
+            return;
+        }
+
+        cv::imwrite(fileDialog.GetPath().ToStdString(), image);
+        SetStatusText(fileDialog.GetFilename() + wxT(" salvo com sucesso!"));
+    }
+
+
+    /*
+     * Quit the application
+     */
+    void MainWindow::Exit(wxCommandEvent& event) {
+        Close(true);
+    }
+
+
+    /*
+     * Show a wxCvResizeDialog and resize @image to the desired size
+     */
+    void MainWindow::ResizeImage(wxCommandEvent& event) {
+
+    }
+
+    void MainWindow::Filter2D(wxCommandEvent& event) {
+
+    }
+
+    void MainWindow::About(wxCommandEvent& event) {
+
     }
 
     wxIMPLEMENT_APP(wxCV);
--- wxcv/src/wxCV.h
+++ wxcv/src/wxCV.h
@@ -5,6 +5,7 @@
 #ifndef ECOM_100_VISAO_COMPUTACIONAL_WXCV_H
 #define ECOM_100_VISAO_COMPUTACIONAL_WXCV_H
 
+#include <opencv2/opencv.hpp>
 #include <wx/app.h>
 #include <wx/frame.h>
 
@@ -17,7 +18,11 @@ namespace wxcv {
     };
 
     class MainWindow : public wxFrame {
+        cv::Mat image;
     public:
+        /*
+         * Widgets id's
+         */
         enum {
             ID_OPEN_IMAGE,
             ID_SAVE_IMAGE,
@@ -25,9 +30,50 @@ namespace wxcv {
             ID_FILTER_2D,
         };
 
+        /*
+         * Main window constructor
+         */
         MainWindow();
 
+        /*
+         * Initialize the menu bar
+         */
         void InitMenu();
+
+        /*
+         * Bind main window's events
+         */
+        void BindEvents();
+
+        /*
+         * Open an image from disk
+         */
+        void OpenImage(wxCommandEvent& event);
+
+        /*
+         * Save @image to disk
+         */
+        void SaveImage(wxCommandEvent& event);
+
+        /*
+         * Quit the application
+         */
+        void Exit(wxCommandEvent& event);
+
+        /*
+         * Resize @image
+         */
+        void ResizeImage(wxCommandEvent& event);
+
+        /*
+         * Apply a 2D filter defined by the user to @image
+         */
+        void Filter2D(wxCommandEvent& event);
+
+        /*
+         * Show about information
+         */
+        void About(wxCommandEvent& event);
     };
 
 }
